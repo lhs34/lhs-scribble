c5a61ff33440fa742d48a67c9dc06b7b83d93620
Heethesh Vhavle <heethesh@gmail.com>
2019-04-30 03:52:26 -0400

    Updated resnet

--- Scribble-OSVOS/networks/resnet.py
+++ Scribble-OSVOS/networks/resnet.py
@@ -192,16 +192,12 @@ class ResNet(nn.Module):
         x = self.relu(x)
         x = self.maxpool(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        l1 = self.layer1(x)
+        l2 = self.layer2(l1)
+        l3 = self.layer3(l2)
+        l4 = self.layer4(l3)
 
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x
+        return l1, l2, l3, l4
 
 
 def resnet18(pretrained=False, **kwargs):
--- Scribble-OSVOS/networks/scribble_net.py
+++ Scribble-OSVOS/networks/scribble_net.py
@@ -14,16 +14,17 @@ class ScribbleNet(nn.Module):
         mask = self.interaction(image, prev_mask, scribble)
         # mask = self.propogation(image, prev_mask, mask)
 
+
 class InteractionNetwork(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.resnet = resnet(pretrained=True)
+        self.resnet = resnet18(pretrained=True)
 
     def forward(self, image, prev_mask, scribble):
-        x = torch.cat((image, prev_mask, scribble))
-        x = resnet(x)
-        return x
+        x = torch.cat((image, prev_mask, scribble), dim=1)
+        l1, l2, l3, l4 = self.resnet(x)
+
 
 class PropogationNetwork(nn.Module):
 
@@ -40,5 +41,6 @@ if __name__ == '__main__':
     model.cuda()
     output = model(image=torch.randn(batch, 3, 256, 256).cuda(),
                    prev_mask=torch.randn(batch, 1, 256, 256).cuda(),
+                   prev_time_mask=torch.randn(batch, 1, 256, 256).cuda(),
                    scribble=torch.randn(batch, 2, 256, 256).cuda())
     print(output.shape)
