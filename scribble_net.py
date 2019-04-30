2dcb6ff88f0010f608516fc2758362dd2070c590
Heethesh Vhavle <heethesh@gmail.com>
2019-04-30 02:55:51 -0400

    Initial outline

---
+++ scribble_net.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class ScribbleNet(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
