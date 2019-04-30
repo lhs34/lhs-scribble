8295acbf5075c4471e0e049f75f5026ae54db692
Sohil <savla.sohil@gmail.com>
2019-04-30 03:41:47 -0400

    Solving merge conflicts

--- Scribble-OSVOS/networks/scribble_net.py
+++ Scribble-OSVOS/networks/scribble_net.py
@@ -33,6 +33,43 @@ class PropogationNetwork(nn.Module):
     def forward(self, image, prev_mask, prev_time_mask):
         pass
 
+class DecoderBlock(nn.Module):
+    def __init__(self, residual_inchannels, residual_outchannels):
+        super(DecoderBlock).__init__()
+        self.residual = nn.ResidualBlock(in_channels=residual_inchannels, out_channels=residual_outchannels)
+        self.upsample = nn.Upsample(scale_factor=2)
+
+    def forward(self, input, skip_connection):
+        skip_input = self.residual(skip_connection)
+        x = self.upsample(input)
+        x = skip_input + x
+        x = self.residual(x)
+        return x
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
 
 if __name__ == '__main__':
     batch = 16
