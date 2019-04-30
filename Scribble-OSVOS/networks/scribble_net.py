12fa81e576b2924a517c795d2edc4beaa421d800
Heethesh Vhavle <heethesh@gmail.com>
2019-04-30 03:35:20 -0400

    Updated outline and loaded resnet

--- Scribble-OSVOS/networks/scribble_net.py
+++ Scribble-OSVOS/networks/scribble_net.py
@@ -1,13 +1,44 @@
 import torch
 import torch.nn as nn
-import torchvision.models as models
+from resnet import *
+
 
 class ScribbleNet(nn.Module):
 
     def __init__(self):
         super().__init__()
-        resnet18 = models.resnet18(pretrained=True)
+        self.interaction = InteractionNetwork()
+        self.propogation = PropogationNetwork()
 
-    def forward(self, x):
-        x = resnet18(x)
+    def forward(self, image, prev_mask, prev_time_mask, scribble):
+        mask = self.interaction(image, prev_mask, scribble)
+        # mask = self.propogation(image, prev_mask, mask)
+
+class InteractionNetwork(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.resnet = resnet(pretrained=True)
+
+    def forward(self, image, prev_mask, scribble):
+        x = torch.cat((image, prev_mask, scribble))
+        x = resnet(x)
         return x
+
+class PropogationNetwork(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, image, prev_mask, prev_time_mask):
+        pass
+
+
+if __name__ == '__main__':
+    batch = 16
+    model = ScribbleNet()
+    model.cuda()
+    output = model(image=torch.randn(batch, 3, 256, 256).cuda(),
+                   prev_mask=torch.randn(batch, 1, 256, 256).cuda(),
+                   scribble=torch.randn(batch, 2, 256, 256).cuda())
+    print(output.shape)
